17fca294eaae42ebdb765f6fc2e0bde9a0de16b3
mjbba-iscteiulpt <mjbba@iscte-iul.pt>
2018-04-30 18:49:57 +0100

    first commit

---
+++ sensorMongo/src/sensorMongo/Main.java
@@ -0,0 +1,17 @@
+package sensorMongo;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import com.mongodb.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+
+	}
+
+}
