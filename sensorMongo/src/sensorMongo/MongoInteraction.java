fde5b9673f8c93c8844467408bf9c9bb9bf110ca
mjbba-iscteiulpt <mjbba@iscte-iul.pt>
2018-05-09 00:41:40 +0100

    .

--- sensorMongo/src/sensorMongo/MongoInteraction.java
+++ sensorMongo/src/sensorMongo/MongoInteraction.java
@@ -7,55 +7,122 @@ import java.util.Map;
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoTimeoutException;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.event.ServerHeartbeatFailedEvent;
+import com.mongodb.event.ServerHeartbeatStartedEvent;
+import com.mongodb.event.ServerHeartbeatSucceededEvent;
+import com.mongodb.event.ServerMonitorListener;
 
-public class MongoInteraction {
+public class MongoInteraction implements ServerMonitorListener{
 
-	MongoCollection<Document> collection;
+	private MongoCollection<Document> collection;
 	private MongoClient mongo;
 	private MongoDatabase database;
-
+	private int sleepTime = 500;
+	
 	public MongoInteraction() {
 
-		// cria novo cliente mongo
-		mongo = new MongoClient("localhost", 27017);
+		try {
+			mongoConnect();
+		} catch (MongoTimeoutException e) {
+			System.out.println("Apanhei!");
+			try {
+				Thread.sleep(returnSleepTime(sleepTime));
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			mongoConnect();
+		}
+	}
 
-		// Creating Credentials
-		MongoCredential credential;
-		credential = MongoCredential.createCredential("sampleUser", "SidMongo", "".toCharArray());
+	public void mongoConnect() throws MongoTimeoutException  {
+	
+            MongoClientOptions clientOptions = new MongoClientOptions.Builder()
+                .addServerMonitorListener((ServerMonitorListener) this)
+                .build();
 
+            mongo = new MongoClient(new ServerAddress("localhost", 27017), clientOptions);
+            mongo.getAddress();
+        
 		System.out.println("Conexão efectuada com sucesso.");
-
-		database = mongo.getDatabase("SidMongo");
-
+		database = mongo.getDatabase("SidMongo");		
 		// Devolver uma colecção existente
 		collection = database.getCollection("SensorTeste");
 		System.out.println("Collection sampleCollection selected successfully");
 	}
-
-	public void insertDocument(Map<String, String> mapMessage) {
-		// Cria um novo documento para ser adicionado ao mongodb
-		// Document document = new Document().append("nome", nome).append("timestamp",
-		// data);
-		Document document = new Document();
-		for (Map.Entry<String, String> entry : mapMessage.entrySet()) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-			document.append(key, value);
+	/*public void mongoConnect() throws Exception {
+		// cria novo cliente mongo
+		mongo = new MongoClient("localhost", 27017);
+		try {
+		mongo.getAddress();
+		} catch (MongoException m) {
+			System.out.println("ASDSAD");
 		}
-		collection.insertOne(document);
-		System.out.println("Document inserted successfully");
+		System.out.println("Conexão efectuada com sucesso.");
+
+		database = mongo.getDatabase("SidMongo");
+		
+		// Devolver uma colecção existente
+		collection = database.getCollection("SensorTeste");
+		System.out.println("Collection sampleCollection selected successfully");
+	}*/
+
+	public void insertDocument(Map<String, String> mapMessage) throws Exception {
+			// Cria um novo documento para ser adicionado ao mongodb
+			// Document document = new Document().append("nome", nome).append("timestamp",
+			// data);
+			Document document = new Document();
+			for (Map.Entry<String, String> entry : mapMessage.entrySet()) {
+				String key = entry.getKey();
+				String value = entry.getValue();
+				document.append(key, value);
+			}
+			collection.insertOne(document);
+			System.out.println("Document inserted successfully");
+			
+		
 	}
 
 	public void getDocument() {
-
 		List<Document> documents = (List<Document>) collection.find().into(new ArrayList<Document>());
-
 		for (Document document : documents) {
 			System.out.println(document);
 		}
+	}
+	
+	
+	public void disconnectMongo() {
+		System.out.println("Desligar ligação com MongoDB.");
+		mongo.close();
+	}
+	
+	public int returnSleepTime(int arg) {
+		sleepTime = arg*2;
+		System.out.println("A tentar reconectar em "+sleepTime/1000+" segundos...");
+		return sleepTime;		
+	}
+
+	@Override
+	public void serverHearbeatStarted(ServerHeartbeatStartedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverHeartbeatSucceeded(ServerHeartbeatSucceededEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverHeartbeatFailed(ServerHeartbeatFailedEvent event) {
+		mongoConnect();
+		
 	}
 
 }
--- sensorMongo/src/sensorMongo/sensorMessages.java
+++ sensorMongo/src/sensorMongo/sensorMessages.java
@@ -1,6 +1,8 @@
 package sensorMongo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -11,41 +13,49 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+
 public class sensorMessages {
 
-	static Map<String, String> mapMessage = new HashMap<String, String>();
-	
 	public static void main(String[] args) {
 
 		String topic = "sid_lab_2018";
 		String broker = "tcp://iot.eclipse.org:1883";
 		String clientId = "JavaSample";
-		MemoryPersistence persistence = new MemoryPersistence();		
-		
+		MemoryPersistence persistence = new MemoryPersistence();
+		List<MqttMessage> pillhaMensagens = new ArrayList<>();
+
 		try {
 			MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
 			MqttConnectOptions connOpts = new MqttConnectOptions();
 			connOpts.setCleanSession(true);
 			System.out.println("Connecting to broker: " + broker);
-			
-			// Listener que fica a espera de mensagens. Desconecta caso a mensagem seja igual a off.
+
+			// Listener que fica a espera de mensagens. Desconecta caso a mensagem seja
+			// igual a off.
 			sampleClient.setCallback(new MqttCallback() {
 				public void connectionLost(Throwable cause) {
 				}
+
 				public void messageArrived(String topic, MqttMessage message) throws Exception {
 					System.out.println("Message: " + message.toString());
-					new MongoInteraction().insertDocument(parseMessage(message));
-					//if (message.toString().equals("off"))
-						//disconnectSensor(sampleClient);						
+					pillhaMensagens.add(message);
+					try {
+						for (MqttMessage i : pillhaMensagens)
+							new MongoInteraction().insertDocument(parseMessage(i));
+						new MongoInteraction().disconnectMongo();
+					} catch (Exception e) {
+						
+					}
 				}
 
 				public void deliveryComplete(IMqttDeliveryToken token) {
 				}
 			});
-			
+
 			sampleClient.connect(connOpts);
 			System.out.println("Connected");
 			sampleClient.subscribe(topic);
+
 		} catch (MqttException me) {
 			System.out.println("reason " + me.getReasonCode());
 			System.out.println("msg " + me.getMessage());
@@ -56,30 +66,22 @@ public class sensorMessages {
 		}
 
 	}
-	
-	
+
 	// Metodo para desconectar.
 	public static void disconnectSensor(MqttClient sampleClient) throws MqttException {
 		System.out.println("Disconnected");
 		sampleClient.disconnect();
 		System.exit(0);
 	}
-	
-	
-	
-	public static Map<String,String> parseMessage(MqttMessage message) {
+
+	public static Map<String, String> parseMessage(MqttMessage message) {
 		String[] messageArray = message.toString().split("\\,", -1);
-		for(String value : messageArray) {
+		Map<String, String> mapMessage = new HashMap<String, String>();
+		for (String value : messageArray) {
 			String[] auxSplit = value.split("\\:", -1);
 			mapMessage.put(auxSplit[0], auxSplit[1]);
 		}
 		return mapMessage;
 	}
-
-
-	public static Map<String, String> getMapMessage() {
-		return mapMessage;
-	}
-	
 	
 }
